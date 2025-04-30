cd91131281123b39b51140caf865ba41ba712c0f
woals2840 <woals2840@gmail.com>
2025-04-30 21:42:05 +0900

    BaseTimeEntity 추가 및 적용

--- src/main/java/org/sopt/domain/post/entity/PostEntity.java
+++ src/main/java/org/sopt/domain/post/entity/PostEntity.java
@@ -1,6 +1,7 @@
 package org.sopt.domain.post.entity;
 
 import org.sopt.domain.user.entity.UserEntity;
+import org.sopt.global.BaseTimeEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -24,7 +25,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 @Table(name = "post")
-public class PostEntity {
+public class PostEntity extends BaseTimeEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
--- src/main/java/org/sopt/domain/user/entity/UserEntity.java
+++ src/main/java/org/sopt/domain/user/entity/UserEntity.java
@@ -1,5 +1,7 @@
 package org.sopt.domain.user.entity;
 
+import org.sopt.global.BaseTimeEntity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,7 +18,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Table(name = "user")
-public class UserEntity {
+public class UserEntity extends BaseTimeEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
